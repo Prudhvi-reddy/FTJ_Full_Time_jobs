1e01c2bd34f91c8e0149b5846c5fdebc2d9ad6ac
prudhvi-reddy <prudhvi.shine@gmail.com>
2025-02-13 04:06:44 -0500

    Used JWT for secure login and logut sessions

--- frontend/login.js
+++ frontend/login.js
@@ -1,31 +1,3 @@
-// document.getElementById("loginForm").addEventListener("submit", async function(event) {
-//     event.preventDefault();
-
-//     let email = document.getElementById("loginEmail").value;
-//     let password = document.getElementById("loginPassword").value;
-
-//     try {
-//         let response = await fetch("http://localhost:5500/login", {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify({ email, password })
-//         });
-
-//         let data = await response.json();
-//         if (response.ok) {
-//             alert(data.message);
-//             window.location.href = "dashboard.html";
-//         } else {
-//             alert(data.error);
-//         }
-//     } catch (error) {
-//         console.error("Error:", error);
-//         alert("Login failed");
-//     }
-// });
-
-
-
 document.getElementById("loginForm").addEventListener("submit", async function(event) {
     event.preventDefault();
 
