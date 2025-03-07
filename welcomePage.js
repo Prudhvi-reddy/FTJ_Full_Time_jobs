4107d01e02397c9d13b9761cb4548f97e84ee3d6
Prudhvi Reddy <55525499+Prudhvi-reddy@users.noreply.github.com>
2025-03-07 02:31:06 -0500

    Update welcomePage.js

--- welcomePage.js
+++ welcomePage.js
@@ -1,7 +1,10 @@
 function redirectToSignup() {
-    window.location.href = '/frontend/signup.html';
+        window.location.href = window.location.origin + '/frontend/signup.html';
+
 }
 
 function redirectToLogin() {
-    window.location.href = '/FTJ_Full_Time_jobs/frontend/login.html';
+    // window.location.href = '/FTJ_Full_Time_jobs/frontend/login.html';
+    window.location.href = window.location.origin + '/frontend/login.html';
+
 }
