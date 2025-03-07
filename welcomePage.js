bed1267202599525e2e83bcba1b89b4195e8f37f
Prudhvi Reddy <55525499+Prudhvi-reddy@users.noreply.github.com>
2025-03-07 02:57:33 -0500

    Update welcomePage.js

--- welcomePage.js
+++ welcomePage.js
@@ -1,8 +1,18 @@
+// function redirectToSignup() {
+//     window.location.href = '/frontend/signup.html';
+// }
+
+// function redirectToLogin() {
+//     // window.location.href = '/FTJ_Full_Time_jobs/frontend/login.html';
+//     window.location.href = '../frontend/login.html';
+// }
+
+
+
 function redirectToSignup() {
-    window.location.href = '/frontend/signup.html';
+    window.location.href = window.location.pathname.replace(/\/$/, '') + '/frontend/signup.html';
 }
 
 function redirectToLogin() {
-    // window.location.href = '/FTJ_Full_Time_jobs/frontend/login.html';
-    window.location.href = '../frontend/login.html';
+    window.location.href = window.location.pathname.replace(/\/$/, '') + '/frontend/login.html';
 }
