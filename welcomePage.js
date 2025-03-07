be25c9fcbc4343463cfc84dc48c096ed12c86e69
prudhvi-reddy <prudhvi.shine@gmail.com>
2025-03-07 03:13:45 -0500

    Update relevant path

--- welcomePage.js
+++ welcomePage.js
@@ -1,14 +1,3 @@
-// function redirectToSignup() {
-//     window.location.href = '/frontend/signup.html';
-// }
-
-// function redirectToLogin() {
-//     // window.location.href = '/FTJ_Full_Time_jobs/frontend/login.html';
-//     window.location.href = '../frontend/login.html';
-// }
-
-
-
 function redirectToSignup() {
     window.location.href = window.location.pathname.replace(/\/$/, '') + '/frontend/signup.html';
 }
